7a357de3c9a1d4fb64103dcffe4752d54dadc291
malikkurosaki <kurosakiblackangel@gmail.com>
2019-01-26 15:51:47 +0800

    malik library

---
+++ mylibrary/src/main/java/com/malikkurosaki/mylibrary/TestToast.java
@@ -0,0 +1,11 @@
+package com.malikkurosaki.mylibrary;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class TestToast {
+
+    public static void toast(Activity activity){
+        Toast.makeText(activity.getApplicationContext(),"halo test",Toast.LENGTH_LONG).show();
+    }
+}
