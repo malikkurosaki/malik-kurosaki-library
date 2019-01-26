006b15da1c6853bd5832b05413a2b261d3a5f3b2
malikkurosaki <kurosakiblackangel@gmail.com>
2019-01-26 16:00:21 +0800

    malik library

--- mylibrary/src/main/java/com/malikkurosaki/mylibrary/TestToast.java
+++ mylibrary/src/main/java/com/malikkurosaki/mylibrary/TestToast.java
@@ -7,5 +7,6 @@ public class TestToast {
 
     public static void toast(Activity activity){
         Toast.makeText(activity.getApplicationContext(),"halo test",Toast.LENGTH_LONG).show();
+        //halo
     }
 }
